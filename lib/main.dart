e21a0d65bc7cba236f0d949ffce878b3251d450e
Dhairysheel-Deshmukh <dhairysheel06@gmail.com>
2021-09-10 18:11:36 +0530

    Added Pages

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,21 @@ import 'package:flutter_iap_project/MasteradminHomeScreen.dart';
 import 'package:flutter_iap_project/MasteradminHome_Profile.dart';
 import 'package:flutter_iap_project/MasteradminLogin.dart';
 import 'package:flutter_iap_project/ViewerLogin.dart';
+import 'package:flutter_iap_project/AdminHomeScreen.dart';
+import 'AddGauge_Page.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     initialRoute: '/opt',
     routes: {
       '/opt': (context) => LoginOptionScrn(),
       '/adminLog': (context) => adminLogin(),
       '/adminHome':(context) => adminHomeScreen(),
       '/ADMINLOGIN' : (context) => AdminLogin(),
-      '/VIEWERLOGIN' : (context) => ViewerLogin()
-
+      '/VIEWERLOGIN' : (context) => ViewerLogin(),
+      '/adminpage' : (context) => Admin_Page(),
+      '/addgauge' : (context) => add_Gauge()
     },
 
     home:LoginOptionScrn(),
